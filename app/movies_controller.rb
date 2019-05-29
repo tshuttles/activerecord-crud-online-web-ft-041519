862b87c04c98c9488e484b977ebb7fd3cefd4ef0
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-29 22:11:10 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,7 +35,7 @@ def can_be_created_in_a_block(title = "Home Alone", release_date = "1990")
 end
 
 def can_get_the_first_item_in_the_database
-  movie.first 
+  movie = Movie.first 
 end
 
 def can_get_the_last_item_in_the_database
