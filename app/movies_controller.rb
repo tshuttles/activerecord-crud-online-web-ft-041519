338393f9f1b2f07f82b62d0322551258f0b2081b
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-29 21:37:57 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -34,19 +34,19 @@ def can_be_created_in_a_block(args = nil)
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  movie.first 
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  movie.last 
 end
 
 def can_get_size_of_the_database
-  __
+  movie.count 
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  movie[0] 
 end
 
 def can_find_by_multiple_attributes
