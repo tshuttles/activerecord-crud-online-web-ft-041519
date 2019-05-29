3689df6976f598ba87b8ba6fe7f8a241f1759582
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-29 22:29:57 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -50,12 +50,12 @@ def can_find_the_first_item_from_the_database_using_id
   movie = Movie.find(1)
 end
 
-def can_find_by_multiple_attributes(args = {title: "Title", release_date: 2000, director: "Me"})
+def can_find_by_multiple_attributes(args = {title: nil, release_date: nil, director: nil})
   # Search Values:
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  movie = Movie.find_by(args)
+  movie = Movie.find_by(args = {title: "Title", release_date: 2000, director: "Me"})
 end
 
 def can_find_using_where_clause_and_be_sorted
