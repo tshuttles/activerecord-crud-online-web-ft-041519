f1775b95b7ed51e441801ebb5e4abe63af909bb9
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-29 21:28:54 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,7 +20,9 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = movie.attributes.save 
+  movie = Movie.new 
+  movie.attributes
+  movie.save 
 end
 
 def can_be_created_in_a_block(args = __)
